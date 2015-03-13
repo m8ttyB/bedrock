9c1e36790b6e8315659c3744f446a40456691929
Alex Gibson <agibson@mozilla.com>
2015-03-13 11:49:24 +0000

    [bug 1134414] Updates to FF37 whatsnew page

--- media/js/firefox/whatsnew-fx37.js
+++ media/js/firefox/whatsnew-fx37.js
@@ -3,40 +3,70 @@
 * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 ;(function($) {
+
+    'use strict';
+
     var $main = $('main');
     var $container = $('.tab-panel');
+    var $smsForm = $('#sms-form');
+    var $smsFormHeading = $('#send-sms > .inner-wrapper > h3');
+    var $smsThankYou = $('.sms-form-thank-you');
+    var $smsInput = $('#number');
 
-    $('.toggle > ul > li > a').on('click', function (e) {
+    var $emailForm = $('#newsletter-form');
+    var $emailThankYou = $('#newsletter-form-thankyou');
+    var $emailInput = $('#id_email');
+
+    $('.toggle > ul > li > a').on('click', function(e) {
         e.preventDefault();
-        var href = e.target.href;
+        var id = e.target.id;
 
         // set the min height of the container should the newsletter
         // have been expanded to avoid content jump.
         $container.css('min-height', $container.height());
 
-        if (href.indexOf('send-sms') !== -1) {
+        if (id === 'tab-sms') {
             $main.attr('data-active', 'sms');
-        } else if (href.indexOf('send-email') !== -1) {
+        } else if (id = 'tab-email') {
             $main.attr('data-active', 'email');
         }
     });
 
-    $('#sms-form').on('submit', function (e) {
+    $smsForm.on('submit', function(e) {
         e.preventDefault();
-        var $form = $(e.target);
-        var action = $form.attr('action');
-        $.post(action, $form.serialize())
-            .done(function (data) {
+        var action = $smsForm.attr('action');
+        $.post(action, $smsForm.serialize())
+            .done(function(data) {
                 // TODO make this fancier like email one
                 if (data.success) {
-                    $form.hide();
-                    $('.sms-form-thank-you').show();
+                    $smsFormHeading.hide();
+                    $smsForm.hide();
+                    $smsThankYou.show();
+                } else if (data.error) {
+                    $smsForm.find('.error').html(data.error).show();
                 }
-                else {
-                    $form.find('.error').html(data.error).show();
-            }})
-            .fail(function () {
-                $form.find('.error').html('We have had a problem. Please try again later.').show();
+            })
+            .fail(function() {
+                $smsForm.find('.error').show();
             });
     });
+
+    $smsThankYou.find('.send-another').on('click', function(e) {
+        e.preventDefault();
+        $smsInput.val('');
+        $smsThankYou.hide();
+        $smsFormHeading.show();
+        $smsForm.show();
+        $smsInput.focus();
+    });
+
+    $emailThankYou.find('.send-another').on('click', function(e) {
+        e.preventDefault();
+        $emailInput.val('');
+        $emailThankYou.hide();
+        $emailForm.show();
+        $emailInput.focus();
+    })
+
+
 })(window.jQuery);
